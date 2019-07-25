c5c66072827afe0634d8e949eed9d4932804834c
Ilayda Türk <34936563+IlaydaTurk@users.noreply.github.com>
2019-07-25 13:35:50 +0300

    Update StudentTestService.java

--- crunchify/restjersey/StudentTestService.java
+++ crunchify/restjersey/StudentTestService.java
@@ -15,55 +15,42 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
-
-
-
-
-
 @Path("/studentservice")
 public class StudentTestService {
-	
 
+	public StudentTestService() {
+		imageclass = new ImageView();
+	}
 
-		public StudentTestService() {
-			imageclass = new ImageView();
-		}
-
-		
 	private ImageView imageclass;
 	private static Student obj;
 
 	@GET
-	//@Produces("application/xml")
+	// @Produces("application/xml")
 	@Path("/testservice")
 	public Response getStudentDetails() throws IOException {
-		
-		
-		
+
 		Student st = new Student();
 		st.setFirstName("ilayda");
 		st.setLastName("Turk");
 		st.setAge(21);
-		byte[] encodedByte = st.convertImage("C:\\Users\\user\\Desktop\\Joker_0026.jpg");
-		
+		byte[] encodedByte = st.convertImage("C:\\Desktop\\Images\\Joker_0026.jpg");
+
 		byte[] decodedByte = Base64.getDecoder().decode(encodedByte);
 		InputStream in = new ByteArrayInputStream(decodedByte);
 		BufferedImage bImageFromConvert = ImageIO.read(in);
 
-		ImageIO.write(bImageFromConvert, "jpg", new File("C:\\Users\\user\\Desktop\\Base64ConvertJoker.jpg"));
-		return Response.status(200).entity(st.getFirstName()+ " " + st.getLastName()+ " " + st.getAge() + " " + encodedByte + imageclass).build();
-		
-		
-		
-	}
-	
-	
-	
+		ImageIO.write(bImageFromConvert, "jpg", new File("C:\\Desktop\\Images\\Base64ConvertJoker.jpg"));
+		return Response.status(200)
+				.entity(st.getFirstName() + " " + st.getLastName() + " " + st.getAge() + " " + encodedByte + imageclass)
+				.build();
 
-	//http://localhost:8080/StudentRestExample/crunchify/studentservice/testservice
-		
+	}
+
+	// http://localhost:8080/ImageProject/crunchify/studentservice/testservice
+
 	@GET
-	//@Produces("application/xml")
+	// @Produces("application/xml")
 	@Path("/displayservice")
 	public Response DisplayStudentDetails(@Context UriInfo info) throws IOException {
 		String firstname = info.getQueryParameters().getFirst("firstname");
@@ -74,11 +61,11 @@ public class StudentTestService {
 		obj.setFirstName(firstname);
 		obj.setLastName(lastname);
 		obj.setAge(Integer.valueOf(age));
-		//byte[] encodedByte=obj.convertImage(file);
-		return Response.status(200).entity("firstname: " + obj.getFirstName() + "lastname:  "
-				+ obj.getLastName() + " age: " + obj.getAge()).build();
-	} 
-	//http://localhost:8080/StudentRestExample/crunchify/studentservice/displayservice?firstname=ilayda&lastname=turk&age=21
-	
-	
+		// byte[] encodedByte=obj.convertImage(file);
+		return Response.status(200).entity(
+				"firstname: " + obj.getFirstName() + "lastname:  " + obj.getLastName() + " age: " + obj.getAge())
+				.build();
+	}
+	// http://localhost:8080/ImageProject/crunchify/studentservice/displayservice?firstname=ilayda&lastname=turk&age=21
+
 }
